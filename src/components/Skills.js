698146b676cb6b6f39e69c63072c5e5bb3956d51
himesh <himesh@cosmoforge.com>
2023-12-14 11:35:44 +0530

    added slide effect to skills slider class

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,114 +8,343 @@ function Skills() {
         id="skills"
         className="skills-container row d-flex container px-3 align-items-start"
       >
-        <h5 className="h5 text-light text-center mb-5 "># SKILLS</h5>
-        <div className="skill-swiper-container container-fluid d-flex my-3 px-5">
-          <div class="skill-swiper-wrapper d-flex justify-content-around gap-5 px-3">
+        <h5 className="h5 text-light text-center mb-3 "># SKILLS</h5>
+        <div className="skill-swiper-container container-fluid my-3 px-5">
+          <div class="skill-swiper-slide justify-content-around gap-5">
             <div className="skills-item">
               <img
                 className="skill-icon"
                 src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
-            </div>
-            <div className="skills-item">
-              <img
-                className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
-              />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2 text-start">ANDROID</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">ARDUINO</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">PHP</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">C</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">C++</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">C#</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">CSS</span>
             </div>
 
             <div className="skills-item">
               <img
                 className="skill-icon"
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg"
+                src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg"
               />
-              <span className="my-2 text-start">WEB DESIGN</span>
-              <p className="separator">
-                To an English person, it will seem like simplified English,told
-                me what{" "}
-              </p>
+              <span className="my-2">DART</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+              />
+              <span className="my-2">PYTHON</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+              />
+              <span className="my-2">JAVA</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              />
+              <span className="my-2">JAVASCRIPT</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg"
+              />
+              <span className="my-2">KOTLIN LANGUAGE</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              />
+              <span className="my-2">HTML5</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/springio/springio-icon.svg"
+              />
+              <span className="my-2">SPRINGBOOT</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg"
+              />
+              <span className="my-2">.NET</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              />
+              <span className="my-2">EXPRESS JS</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-plain-wordmark.svg"
+              />
+              <span className="my-2">LARAVEL</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Ionic_Logo.svg"
+              />
+              <span className="my-2">IONIC</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+              />
+              <span className="my-2">BOOTSTRAP</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg"
+              />
+              <span className="my-2">FLUTTER</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg"
+              />
+              <span className="my-2">PANDAS</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              />
+              <span className="my-2">REACT JS</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://reactnative.dev/img/header_logo.svg"
+              />
+              <span className="my-2">REACT NATIVE</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/selenium-logo.svg"
+              />
+              <span className="my-2">SELENIUM</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg"
+              />
+              <span className="my-2">MS SQL SERVER</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+              />
+              <span className="my-2">MY SQL SERVER</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+              />
+              <span className="my-2">POSTGRE SQL</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg"
+              />
+              <span className="my-2">ORACLE</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/bestofjs/bestofjs-webui/8665e8c267a0215f3159df28b33c365198101df5/public/logos/realm.svg"
+              />
+              <span className="my-2">REALM</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg"
+              />
+              <span className="my-2">SQLITE</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+              />
+              <span className="my-2">MONGO DB</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg"
+              />
+              <span className="my-2">GOOGLE CLOUD</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
+              />
+              <span className="my-2">AWS</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+              />
+              <span className="my-2">FIREBASE</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              />
+              <span className="my-2">NODE JS</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+              />
+              <span className="my-2">GIT</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg"
+              />
+              <span className="my-2">FIGMA</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+              />
+              <span className="my-2">DOCKER</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
+              />
+              <span className="my-2">HEROKU</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
+              />
+              <span className="my-2">LINUX</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png"
+              />
+              <span className="my-2">MATLAB</span>
+            </div>
+
+            <div className="skills-item">
+              <img
+                className="skill-icon"
+                src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+              />
+              <span className="my-2">POSTMAN</span>
             </div>
           </div>
         </div>
